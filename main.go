e8b7fd772a10162461ceb5af8b8dfd105f00cff5
John Flowers <jeflowers@gmail.com>
2021-01-05 23:16:39 -0600

    modify Person struct to include lastname and format as json content type

--- main.go
+++ main.go
@@ -4,63 +4,42 @@ import (
 	"encoding/json"
 	"fmt"
 	"log"
+
+	//"log"
 	"net/http"
 )
 
-type person struct {
-	First string
+type Person struct {
+	Firstname string `json:"firstname"`
+	Lastname string `json:"lastname"`
 }
 func main(){
-	p1 := person{
-		First: "Evadne",
-	}
-	p2 := person{
-		First: "Tennille",
-	}
 
-	xp := []person{p1,p2}
-	bs, err := json.Marshal(xp)
-	if err != nil {
-		log.Panic(bs)
-	}
-	fmt.Println("PRINT JS",string(bs))
 
-	xp2 :=  []person{}
-	err = json.Unmarshal(bs, &xp2)
-	if err != nil {
-		log.Panic(err)
-	}
-	fmt.Println("Back into a go data structure", xp2)
+	http.HandleFunc("/decode", func(w http.ResponseWriter, r *http.Request){
+		var user Person
+		err := json.NewDecoder(r.Body).Decode(&user)
+		if err != nil{
+			log.Println("decode error")
+		}
 
-	http.HandleFunc("/encode", foo)
-	http.HandleFunc("/decode", bar)
+		fmt.Fprintf(w, "%s %s is a good worker", user.Firstname, user.Lastname)
+	})
+
+
+	http.HandleFunc("/encode", func(w http.ResponseWriter, r *http.Request){
+		statement := Person{
+			Firstname: "John",
+			Lastname: "Doe",
+		}
+
+		err := json.NewEncoder(w).Encode(statement)
+		if err != nil{
+			log.Println("Bad data to encode, printing out help statement", err)
+		}
+	})
 	http.ListenAndServe(":8090", nil)
 }
 
-func foo(w http.ResponseWriter, r *http.Request){
-	p1 := person{
-		First: "Evadne",
-	}
 
-	// Write to response writer
-	err := json.NewEncoder(w).Encode(p1) // encode data to write to ResponseWriter
-	// Check the error
-	if err != nil {
-		log.Println("Encoded bad data", err)
-	}
-
-}
-
-func bar(w http.ResponseWriter, r *http.Request){
-
-	var p1 person
-	err := json.NewDecoder(r.Body).Decode(&p1)  // decode data to read from ResponseWriter
-	if err != nil {
-		log.Println(
-			"Decoded bad data",
-			err,
-		)
-	}
-	log.Println("Person: ", p1)
-}
 
