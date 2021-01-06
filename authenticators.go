dd27118a4a032446e9021256723cec83e045fcea
John Flowers <jeflowers@gmail.com>
2021-01-06 02:00:26 -0600

    add hmac functions to sign messages and check signature

--- authenticators.go
+++ authenticators.go
@@ -1,13 +1,22 @@
 package main
 
 import (
+	"crypto/hmac"
+	"crypto/sha512"
 	"fmt"
 	"golang.org/x/crypto/bcrypt"
 	"log"
 )
 
+var key = []byte{}
+
 func main(){
 	//fmt.Println(base64.StdEncoding.EncodeToString([]byte("user:pass")))
+
+	//hmac implementation
+	for i := 1; i <= 64; i++{
+		key = append(key, byte(i))
+	}
 	passwrd := "God!sGoodT0m3"
 	hashedPasswrd, err := hashPassword(passwrd)
 	if err != nil{
@@ -20,6 +29,7 @@ func main(){
 	log.Println("Logged in!")
 }
 
+/* bcrypt functions*/
 func hashPassword(password string)([]byte, error) {
 	bs, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
 	if err != nil {
@@ -34,4 +44,37 @@ func comparePassword(password string, hashedPass []byte) error {
 		return fmt.Errorf("Invalid password: %w", err)
 	}
 	return nil
+}
+
+/* Bearer tokens and HMAC
+   	Bearer tokens: Uses authorization head & keyword "bearer"
+   	Used for: prevent faked bearer tokens
+   	Used in:  Cryptographic signing --> Way to prove the value was created/validated by a
+             specific person
+	HMAC -- Hash Message Authentication Code  --> https://godoc.org/crypto/hmac
+	In general HMAC is a cryptographic signing algorithm
+*/
+func signMessage(msg []byte)([]byte, error){
+	h := hmac.New(sha512.New, key)
+	 _, err := h.Write(msg)
+	 if err != nil{
+	 	return nil, fmt.Errorf("Error while hashing message %w", err)
+	 }
+	 signature := h.Sum(nil)
+	 return signature, nil
+}
+
+/* Check signature */
+func checkSig(msg, sig []byte)(bool, error)  {
+	//first sign the message
+	newSig, err := signMessage(msg)
+	if err != nil{
+		return false, fmt.Errorf("Error in checkSig while getting signature of message: %w", err)
+	}
+
+	/* Check if true or false*/
+	same := hmac.Equal(newSig, sig)
+	return same, nil
+
+
 }
